f9c3987ad7216ea56b8b97959171273bc5b29db9
nadavo <nadavo@gmail.com>
2017-01-29 03:29:40 +0200

    Add loadMyModel.lua file

---
+++ loadMyModel.lua
@@ -0,0 +1,70 @@
+require 'torch'
+require 'image'
+require 'nn'
+require 'cunn'
+require 'cudnn'
+require 'optim'
+
+local trainset = torch.load('cifar.torch/cifar10-train.t7')
+local testset = torch.load('cifar.torch/cifar10-test.t7')
+
+local classes = {'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'}
+
+local trainData = trainset.data:float() -- convert the data from a ByteTensor to a float Tensor.
+local trainLabels = trainset.label:float():add(1)
+local testData = testset.data:float()
+local testLabels = testset.label:float():add(1)
+
+-- Load and normalize data:
+
+local mean = {}  -- store the mean, to normalize the test set in the future
+local stdv  = {} -- store the standard-deviation for the future
+for i=1,3 do -- over each image channel
+    mean[i] = trainData[{ {}, {i}, {}, {}  }]:mean() -- mean estimation
+    print('Channel ' .. i .. ', Mean: ' .. mean[i])
+    trainData[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+
+    stdv[i] = trainData[{ {}, {i}, {}, {}  }]:std() -- std estimation
+    print('Channel ' .. i .. ', Standard Deviation: ' .. stdv[i])
+    trainData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+-- Normalize test set using same values
+
+for i=1,3 do -- over each image channel
+    testData[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+    testData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+local batchSize = 64
+local optimState = {}
+model = torch.load('network.model')
+criterion = nn.ClassNLLCriterion():cuda()
+
+function forwardNet(data,labels)
+    --another helpful function of optim is ConfusionMatrix
+    local confusion = optim.ConfusionMatrix(classes)
+    local lossAcc = 0
+    local numBatches = 0
+    model:evaluate() -- turn off drop-out
+    for i = 1, data:size(1) - batchSize, batchSize do
+        numBatches = numBatches + 1
+        local x = data:narrow(1, i, batchSize):cuda()
+        local yt = labels:narrow(1, i, batchSize):cuda()
+        local y = model:forward(x)
+        local err = criterion:forward(y, yt)
+        lossAcc = lossAcc + err
+        confusion:batchAdd(y,yt)
+    end
+
+    confusion:updateValids()
+    local avgError = 1 - confusion.totalValid
+
+    return avgError
+end
+
+---------------------------------------------------------------------
+
+testError = forwardNet(testData, testLabels)
+
+return testError
